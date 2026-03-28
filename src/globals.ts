b7a7a45fcc5d65c8727671c7c2def05111795f4c
Ot92 <onalethatatautona@gmail.com>
2026-03-28 18:17:21 +0200

    Update globals.ts

--- src/globals.ts
+++ src/globals.ts
@@ -1,9 +1,16 @@
+export const REDIS_HOST = process.env.REDIS_HOST as string;
+export const REDIS_PORT = process.env.REDIS_PORT as string;
+export const REDIS_PASSWORD = process.env.REDIS_PASSWORD as string;
+export const INSTANCE_ID = process.env.INSTANCE_ID || process.env.HOSTNAME || 'scheduler';
+
 export const SERVICE_URLS: {[key: string]: string} = {
-  ivdmsServiceURI: "http://localhost:6666/api/cron"
+  ivdmsServiceURI: process.env.IVDMS_SERVICE_URI || "http://localhost:8888/api/cron"
 };
 
 export const webhookTypes = {
-  trackApplicationWorkflow: "track-application-work-flow",
-  trackRenewMotorVehicleLicenseApplication: "track-renew-motor-vehicle-license-application",
-  trackMotorVehicleLicenseCertificate: "track-motor-vehicle-license-certificate",
+  trackExaminationExpiry: "track-examination-expiry",
+  trackProcessOutputDocumentExpiry: "track-process-output-document-expiry",
+  trackProcessOutputDocumentForPenaltyFees: "track-process-output-document-for-penalty-fees",
+  trackPaymentLockExpiry: "track-payment-lock-expiry",
+  refreshOcasComplianceScores: "refresh-ocas-compliance-scores",
 };
